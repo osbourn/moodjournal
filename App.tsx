161245770c73276562788b8fafd63422b967e89e
Niels Voss <code@nielsvoss.com>
2022-10-02 15:22:56 -0700

    Use Native Stack Navigator

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
